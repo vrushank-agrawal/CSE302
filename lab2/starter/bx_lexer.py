535903db270a1399152c3f9ebf3a2205ac21e860
vrushank-agrawal <vrushank2001@gmail.com>
2022-09-23 18:05:42 +0200

    update lexer

--- lab2/starter/bx_lexer.py
+++ lab2/starter/bx_lexer.py
@@ -1,32 +1,39 @@
 import argparse, sys, json
-from tokenize import Token
+from typing import List
 from py.ply import lex as lex
 
 lst = []
 
 class MyLexer(object):
 
+    def __init__(self, code: str) -> None:
+        self.lexer = lex.lex(module=self)
+        self.code_data: str = code 
+        self.token_list: List = []
+        self.test()
+
     reserved = {
         'print':    "PRINT",
         'def':      "DEF",
         'main':     "MAIN",
-        'int':      "INT"
+        'int':      "INT",
+        'var':      "Var"
     }
 
     # List of token names.   This is always required
     tokens = (
         'IDENT', 'NUMBER',
-        'SEMICOLON', 'EQUAL', 'LPAREN', 'RPAREN', 'COLON', 'RBRACE', 'LBRACE',
-        'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULUS', 'OPPOSITE',
+        'SEMICOLON', 'EQUAL', 'COLON', 'LPAREN', 'RPAREN', 'RBRACE', 'LBRACE', 'EOF',
+        'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULUS',
         'BITWISE_AND', 'BITWISE_OR', 'BITWISE_XOR', 'BITWISE_SHL', 'BITWISE_SHR', 'BITWISE_NOT'
     ) + tuple(reserved.values())
 
     # MACROS
-    t_SEMICOLON = r'\;'
-    t_EQUAL     = r'\='
+    t_SEMICOLON = r';'
+    t_EQUAL     = r'='
+    t_COLON     = r':'
     t_LPAREN    = r'\('
     t_RPAREN    = r'\)'
-    t_COLON     = r'\:'
     t_RBRACE    = r'\{'
     t_LBRACE    = r'\}'
 
@@ -34,24 +41,18 @@ class MyLexer(object):
     t_MINUS     = r'-'
     t_TIMES     = r'\*'
     t_DIVIDE    = r'/'
-    t_MODULUS   = r'\%'
-    t_OPPOSITE  = r'\-'
+    t_MODULUS   = r'%'
 
-    t_BITWISE_AND   = r'\&'
+    t_BITWISE_AND   = r'&'
     t_BITWISE_OR    = r'\|'
     t_BITWISE_XOR   = r'\^'
-    t_BITWISE_SHL   = r'\<\<'
-    t_BITWISE_SHR   = r'\>\>'
-    t_BITWISE_NOT   = r'\~'
+    t_BITWISE_SHL   = r'<<'
+    t_BITWISE_SHR   = r'>>'
+    t_BITWISE_NOT   = r'~'
 
     # For token identifiers
-    zero             = r'(0)'
-    digit            = r'([0-9])'
-    nonzero_digit    = r'([1-9])'
-    nondigit         = r'([A-Za-z])'
-    underscore       = r'\_'
-    id_identifier    = r'(' + nondigit + r'(' + digit + r'|' + nondigit + r'|' + underscore + r')*)'
-    num_identifier   = r'(' + zero + r'|' + r'(' + nonzero_digit + r'(' + digit + r')*))'
+    id_identifier    = r'[A-Za-z_][A-Za-z0-9_]*'
+    num_identifier   = r'0|[1-9][0-9]*'
 
     @lex.TOKEN(id_identifier)
     def t_IDENT(self, t):
@@ -65,40 +66,41 @@ class MyLexer(object):
         t.value = int(t.value)
         return t
 
-    # A string containing ignored characters (spaces and tabs)
-    t_ignore  = ' \t'
-
-    def t_COMMENT(self, t):
+    def t_COMMENT(self, t) -> None:
         """ Ignores all characters until newline character """
-        r'//.*\n'
+        r'//.*\n?'
 
-    def t_newline(self, t):
-        """ Ignores the character and augments the line number """
-        r'\n+'
-        t.lexer.lineno += len(t.value)
+    def t_eof(self, t) -> None:
+        """ EOF handler """
+        return None
 
     def find_column(input, token):
         """ Finds the column number of the pointer """
         line_start = input.rfind('\n', 0, token.lexpos) + 1
         return (token.lexpos - line_start) + 1
 
-    def t_error(self, t):
+    def t_error(self, t) -> None:
         """ Prints an error message on unsuccessful line parsing """
+        if t.value[0] == '\n': print("newline")
         print("Illegal character '%s' at line '%s'" % (t.value[0], t.lexer.lineno))
         t.lexer.skip(1)
         sys.exit(1)
 
-    def build(self,**kwargs):
-        """ Function to buld the lexer """
-        self.lexer = lex.lex(module=self, **kwargs)
-    
-    def test(self, data):
+    def t_newline(self, t) -> None:
+        """ Ignores the newline character and augments the line number """
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore  = ' \t\r\f\v'
+
+    def test(self) -> None:
         """ Runs the lexer to tokenize all code """
-        self.lexer.input(data)
+        self.lexer.input(self.code_data)
         while True:
-             tok = self.lexer.token()
-             if tok:
-                lst.append(tok)
+            tok = self.lexer.token()
+            if tok:
+                self.token_list.append(tok)
                 print(tok)
 
 
@@ -115,7 +117,6 @@ if __name__=="__main__":
 
     # print(code)
 
-    m = MyLexer()
-    m.build()
-    m.test(code)
+    m = MyLexer(code)
+    # m.test(code)
     
